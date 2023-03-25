60beca59516962d45bb1dabf33412fe7ea575fd3
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2023-03-24 23:29:00 -0300

    feat: working with queries

--- src/main/kotlin/br/com/alura/forum/repository/TopicoRepository.kt
+++ src/main/kotlin/br/com/alura/forum/repository/TopicoRepository.kt
@@ -4,9 +4,13 @@ import br.com.alura.forum.model.Topico
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface TopicoRepository : JpaRepository<Topico, Long> {
     fun findByCursoNome(nomeCurso: String, paginacao: Pageable) : Page<Topico>
+
+    @Query("select t from Topico t where t.respostas is empty")
+    fun topicosNaoRespondidos(): List<Topico>
 }
